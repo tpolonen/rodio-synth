1b0b0b93d4e2548771f789b9db0c49f008e998d8
tpolonen <tpolonen@student.hive.fi>
2022-05-27 14:15:40 +0300

    we have correct waveforms and beeps

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use core::time::Duration;
 use rodio::{OutputStream, source::Source, Sink};
 
+const VOL_MULTIPLIER: f32 = 0.5;
+
+struct Note {
+	pitch: f32,
+	duration: f32,
+}
+
+struct Track {
+	oscillator: WavetableOscillator,
+	sink: Sink,
+	volume: f32,
+	notes: Vec<Note>,
+}
+
 #[derive(Clone)]
 struct WavetableOscillator {
 	sample_rate: u32,
@@ -77,59 +91,130 @@ fn main() {
 	let mut square_table: Vec<f32> = Vec::with_capacity(wave_table_size);
 	let mut triangle_table: Vec<f32> = Vec::with_capacity(wave_table_size);
 
-	let saw_iterations = 100;
-	let triangle_iterations = 100;
+	let saw_iterations = 10;
+	let triangle_iterations = 10;
 
 	for n in 1..wave_table_size {
 		sine_table.push((2.0 * std::f32::consts::PI * n as f32 / wave_table_size as f32).sin());
 	}
 
+	println!("sine table");
+	for n in &sine_table {
+		println!("{}", n);
+	}
+
 	for n in 0..wave_table_size {
-		saw_table.push( {
-			let mut sum = 0.0;
-			for i in 1..saw_iterations {
-				sum += (i as f32 * 2.0 * std::f32::consts::PI * n as f32 / wave_table_size as f32).sin() + 1.0 / (1.0 + i as f32);
-				}
-			sum
+		saw_table.push( -1.0 + (2.0 / wave_table_size as f32) * n as f32 );
+	}
+
+	println!("saw table");
+	for n in &saw_table {
+		println!("{}", n);
+	}
+
+	for n in 0..wave_table_size {
+		square_table.push(if (2.0 * std::f32::consts::PI * n as f32 / wave_table_size as f32).sin() >= 0.0 { 1.0 } else { -1.0});
+	}
+
+	println!("square table");
+	for n in &square_table {
+		println!("{}", n);
+	}
+
+	for n in 0..wave_table_size {
+		triangle_table.push( { if n < wave_table_size / 2 { 
+			-1.0 + (2.0 / wave_table_size as f32) * n as f32 * 2.0 }
+		else { 3.0 - (2.0 / wave_table_size as f32) * n as f32 * 2.0 }
 		} );
 	}
 
-	for n in 0..wave_table_size {
-		square_table.push(if (2.0 * std::f32::consts::PI * n as f32 / wave_table_size as f32).sin() > 0.0 { 1.0 } else { -1.0});
+	println!("triangle table");
+	for n in &triangle_table {
+		println!("{}", n);
 	}
 
-	for n in 0..wave_table_size {
-		triangle_table.push( {
-			let mut sum = 0.0;
-			for i in (1..triangle_iterations).step_by(2) {
-				sum += (if i == 3 { -1.0 } else { 1.0 }) * (i as f32 * 2.0 * std::f32::consts::PI * n as f32 / wave_table_size as f32).sin() + f32::powf(i as f32 + 2.0, -2.0)
-				}
-			sum
-		} );
+	let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+
+	let triangle_notes: Vec<Note> = vec![
+		Note {
+			pitch: 261.63,
+			duration: 0.5,
+		},
+		Note {
+			pitch: 293.66,
+			duration: 0.5,
+		},
+		Note {
+			pitch: 329.63,
+			duration: 0.5,
+		},
+		Note {
+			pitch: 349.23,
+			duration: 0.5,
+		},
+		Note {
+			pitch: 329.63,
+			duration: 0.5,
+		},
+		Note {
+			pitch: 293.66,
+			duration: 0.5,
+		},
+	];
+
+	let sine_notes: Vec<Note> = vec![
+		Note {
+			pitch: 261.63,
+			duration: 1.5,
+		},
+		Note {
+			pitch: 293.66,
+			duration: 1.5,
+		},	
+	];
+
+ 	let sink = Sink::try_new(&stream_handle).unwrap();
+	let sink2 = Sink::try_new(&stream_handle).unwrap();
+	let sine_oscillator = WavetableOscillator::new(44100, saw_table);
+	let triangle_oscillator = WavetableOscillator::new(44100, square_table);
+
+	let mut tracks: Vec<Track> = vec![
+		Track {
+			oscillator: triangle_oscillator.clone(),
+			sink: sink,
+			volume: 0.32,
+			notes: triangle_notes,
+		},
+		Track {
+			oscillator: sine_oscillator.clone(),
+			sink: sink2,
+			volume: 0.32,
+			notes: sine_notes,
+		},
+	];
+
+	for track in tracks.iter_mut() {
+		track.sink.set_volume(track.volume * VOL_MULTIPLIER);
+		for note in track.notes.iter() {
+			track.oscillator.set_frequency(note.pitch);
+			track.sink.append(track.oscillator.clone().take_duration(std::time::Duration::from_secs_f32(note.duration)))
+		}
 	}
 
-	/* println!("sine");
-	let mut sine_oscillator = WavetableOscillator::new(44100, sine_table);
-	sine_oscillator.set_frequency(220.0);
-	sine_oscillator.set_frequency(120.0);
-	let sine_beat = sine_oscillator.clone().take_duration(std::time::Duration::from_secs(1));
-	let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-	let sink = Sink::try_new(&stream_handle).unwrap();
-	let mut saw_oscillator = WavetableOscillator::new(44100, saw_table);
-	saw_oscillator.set_frequency(440.0);
-	sink.append(saw_oscillator.take_duration(std::time::Duration::from_secs(1)));
-	let new_beat = sine_beat.clone();
-	sink.append(sine_oscillator.clone().take_duration(std::time::Duration::from_secs(1)));
-	sink.append(sine_oscillator.clone().take_duration(std::time::Duration::from_secs(1)));
-	sink.set_volume(0.5);
-	sink.sleep_until_end(); */
+	std::thread::sleep(std::time::Duration::from_secs(3));
 
-	let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-	let sink = Sink::try_new(&stream_handle).unwrap();
+
+/* 	let sink = Sink::try_new(&stream_handle).unwrap();
 	let sink2 = Sink::try_new(&stream_handle).unwrap();
 
-	let mut sine_oscillator = WavetableOscillator::new(44100, sine_table);
-	let mut triangle_oscillator = WavetableOscillator::new(44100, triangle_table);
+	let instrument1_multiplier = 0.42;
+	let instrument2_multiplier = 0.62;
+    sink.set_volume(VOL_MULTIPLIER * instrument1_multiplier);
+    sink2.set_volume(VOL_MULTIPLIER * instrument2_multiplier);
+    
+
+	let mut sine_oscillator = WavetableOscillator::new(44100, triangle_table);
+	let mut triangle_oscillator = WavetableOscillator::new(44100, sine_table);
 	//C
 	sine_oscillator.set_frequency(261.63);
 	sink.append(sine_oscillator.clone().take_duration(std::time::Duration::from_secs_f32(0.5)));
@@ -160,6 +245,6 @@ fn main() {
 	sink2.set_volume(0.5);
 	sink.sleep_until_end();
 	sink2.sleep_until_end();
-	
+ */
 //	std::thread::sleep(std::time::Duration::from_secs(2));
 }
